1d09b2d8ec46eab5f8bc9fd3ccc155774f4317f0
hamzalamin <hamzalamin80@gmail.com>
2024-10-28 13:55:55 +0100

    Initial project setup

---
+++ src/test/java/com/wora/itlens/ItLensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wora.itlens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
