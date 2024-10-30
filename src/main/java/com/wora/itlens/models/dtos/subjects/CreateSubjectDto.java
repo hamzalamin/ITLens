e4feebf64c0b44055bd8a897ad797044c69732e7
hamzalamin <hamzalamin80@gmail.com>
2024-10-30 13:30:13 +0100

    feat(dto): implement CreateSubjectDto for adding new subject data, linked to survey edition and parent subject

--- src/main/java/com/wora/itlens/models/dtos/subjects/CreateSubjectDto.java
+++ src/main/java/com/wora/itlens/models/dtos/subjects/CreateSubjectDto.java
@@ -1,4 +1,12 @@
 package com.wora.itlens.models.dtos.subjects;
 
-public class CreateSubjectDto {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record CreateSubjectDto(
+        @NotBlank String title,
+        @NotNull @Positive Long surveyEdition,
+        @NotNull @Positive Long subject
+) {
 }
