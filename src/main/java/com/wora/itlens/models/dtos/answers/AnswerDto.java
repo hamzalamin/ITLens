a26e723496c410e0d6099bb145c78ae5220864ba
hamzalamin <hamzalamin80@gmail.com>
2024-10-30 10:43:19 +0100

    feat(dto): implement AnswerDto class with validation constraints

---
+++ src/main/java/com/wora/itlens/models/dtos/answers/AnswerDto.java
@@ -0,0 +1,14 @@
+package com.wora.itlens.models.dtos.answers;
+
+import com.wora.itlens.models.entites.Question;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record AnswerDto(
+        @Positive @NotNull Long id,
+        @NotBlank String text,
+        @Positive @NotNull Integer selectionCount,
+        Question question
+) {
+}
