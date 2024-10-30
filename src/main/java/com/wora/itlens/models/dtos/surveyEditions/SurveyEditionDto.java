49f02d9a941e3b749f0b3d2d9765bd2b2e91e331
hamzalamin <hamzalamin80@gmail.com>
2024-10-30 13:32:39 +0100

    feat(dto): implement SurveyEditionDto to represent survey edition details, including creation and start dates, survey reference, and associated subjects

--- src/main/java/com/wora/itlens/models/dtos/surveyEditions/SurveyEditionDto.java
+++ src/main/java/com/wora/itlens/models/dtos/surveyEditions/SurveyEditionDto.java
@@ -1,15 +1,20 @@
 package com.wora.itlens.models.dtos.surveyEditions;
 
+import com.wora.itlens.models.dtos.subjects.EmbeddedSubjectDto;
+import com.wora.itlens.models.dtos.surveys.EmbeddedSurveyDto;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.time.LocalDate;
+import java.util.List;
 
-public record EmbeddedSurveyEditionsDto(
+public record SurveyEditionDto(
         @NotNull @Positive Long id,
         @NotNull @Future LocalDate creationDate,
         @NotNull @Future LocalDate startDate,
-        @NotNull @Future LocalDate date
+        @NotNull @Future LocalDate date,
+        EmbeddedSurveyDto survey,
+        List<EmbeddedSubjectDto> subjects
 ) {
 }
