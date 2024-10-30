1e29b9d8d80dfda6ccc006b75a2b7cd935b5926b
hamzalamin <hamzalamin80@gmail.com>
2024-10-31 00:59:23 +0100

    feat: Implement SurveyMapper interface
    - Added SurveyMapper interface extending GenericMapper for mapping between Survey entities and DTOs.
    - Included methods to convert CreateSurveyDto and UpdateSurveyDto to Survey entity and vice versa.

---
+++ src/main/java/com/wora/itlens/mappers/SurveyMapper.java
@@ -0,0 +1,15 @@
+package com.wora.itlens.mappers;
+
+import com.wora.itlens.mappers.api.GenericMapper;
+import com.wora.itlens.models.dtos.surveys.CreateSurveyDto;
+import com.wora.itlens.models.dtos.surveys.SurveyDto;
+import com.wora.itlens.models.dtos.surveys.UpdateSurveyDto;
+import com.wora.itlens.models.entites.Survey;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface SurveyMapper extends GenericMapper<Survey, SurveyDto> {
+    Survey toEntity(CreateSurveyDto createSurveyDto);
+    Survey toEntity(UpdateSurveyDto updateSurveyDto);
+    SurveyDto toDto(Survey survey);
+}
