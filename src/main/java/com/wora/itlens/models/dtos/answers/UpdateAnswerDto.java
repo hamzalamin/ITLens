e33b2dca25c4dd0df8874ace72a6796d04ee08e7
hamzalamin <hamzalamin80@gmail.com>
2024-10-30 10:49:26 +0100

    feat(dto): implement UpdateAnswerDto class for answer modification

---
+++ src/main/java/com/wora/itlens/models/dtos/answers/UpdateAnswerDto.java
@@ -0,0 +1,12 @@
+package com.wora.itlens.models.dtos.answers;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record UpdateAnswerDto(
+        @NotBlank String text,
+        @Positive @NotNull Integer selectionCount,
+        @NotNull @Positive Long questionId
+) {
+}
