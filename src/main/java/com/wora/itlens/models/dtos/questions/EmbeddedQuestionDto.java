7d542a9929414f56c63f7b9de24297515132b7d1
hamzalamin <hamzalamin80@gmail.com>
2024-10-30 10:54:48 +0100

    feat(dto): implement EmbeddedQuestionDto class for embedded question representation

---
+++ src/main/java/com/wora/itlens/models/dtos/questions/EmbeddedQuestionDto.java
@@ -0,0 +1,14 @@
+package com.wora.itlens.models.dtos.questions;
+
+import com.wora.itlens.models.enumes.QuestionType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record EmbeddedQuestionDto(
+        @NotNull @Positive Long id,
+        @NotBlank String text,
+        @NotNull @Positive Integer answerCount,
+        @NotNull QuestionType questionType
+) {
+}
