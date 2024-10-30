03f96a621e0f2631051e20f8fb58382cbacb6380
hamzalamin <hamzalamin80@gmail.com>
2024-10-30 11:14:17 +0100

    feat(dto): implement QuestionsDto class for representing questions with associated answers and subject

---
+++ src/main/java/com/wora/itlens/models/dtos/questions/QuestionsDto.java
@@ -0,0 +1,20 @@
+package com.wora.itlens.models.dtos.questions;
+
+import com.wora.itlens.models.dtos.answers.EmbeddedAnswerDto;
+import com.wora.itlens.models.dtos.subjects.EmbeddedSubjectDto;
+import com.wora.itlens.models.enumes.QuestionType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.util.List;
+
+public record QuestionsDto(
+        @NotNull @Positive Long id,
+        @NotBlank String text,
+        @NotNull @Positive Integer answerCount,
+        @NotNull QuestionType questionType,
+        EmbeddedSubjectDto subject,
+        List<EmbeddedAnswerDto> answers
+) {
+}
