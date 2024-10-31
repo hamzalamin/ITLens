87ac3413634076995177823b00b1f4bbaa7cc2f2
hamzalamin <hamzalamin80@gmail.com>
2024-10-31 11:06:45 +0100

    feat: Add createSurveyEdition endpoint
    - Implemented POST /survey/edition to create a new survey edition.
    - Utilized ISurveyEditionService to handle the creation logic.

--- src/main/java/com/wora/itlens/controllers/SurveyEditionController.java
+++ src/main/java/com/wora/itlens/controllers/SurveyEditionController.java
@@ -1,7 +1,14 @@
 package com.wora.itlens.controllers;
 
+import com.wora.itlens.models.dtos.surveyEditions.CreateSurveyEditionDto;
+import com.wora.itlens.models.dtos.surveyEditions.SurveyEditionDto;
 import com.wora.itlens.services.interfaces.ISurveyEditionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SurveyEditionController {
     private final ISurveyEditionService surveyEditionService;
 
+    @PostMapping
+    public ResponseEntity<SurveyEditionDto> createSurveyEdition(@RequestBody @Valid CreateSurveyEditionDto createSurveyEditionDto){
+        return new ResponseEntity<>(surveyEditionService.save(createSurveyEditionDto), HttpStatus.OK);
+    }
 }
