bb00c9bc386bbe9f07b5cf403e222d69940a97e4
hamzalamin <hamzalamin80@gmail.com>
2024-10-31 09:07:28 +0100

    feat: Add SurveyEditionRepository for managing SurveyEdition entities
    - Created SurveyEditionRepository interface extending JpaRepository to handle CRUD operations for SurveyEdition entities.

---
+++ src/main/java/com/wora/itlens/repositories/SurveyEditionRepository.java
@@ -0,0 +1,7 @@
+package com.wora.itlens.repositories;
+
+import com.wora.itlens.models.entites.SurveyEdition;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SurveyEditionRepository extends JpaRepository<SurveyEdition, Long> {
+}
---
+++ src/main/java/com/wora/itlens/services/impl/SurveyEditionService.java
@@ -0,0 +1,40 @@
+package com.wora.itlens.services.impl;
+
+import com.wora.itlens.models.dtos.surveyEditions.CreateSurveyEditionDto;
+import com.wora.itlens.models.dtos.surveyEditions.SurveyEditionDto;
+import com.wora.itlens.models.dtos.surveyEditions.UpdateSurveyEditionDto;
+import com.wora.itlens.services.interfaces.ISurveyEditionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SurveyEditionService implements ISurveyEditionService {
+
+    @Override
+    public SurveyEditionDto save(CreateSurveyEditionDto createSurveyEditionDto) {
+        return null;
+    }
+
+    @Override
+    public SurveyEditionDto findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public SurveyEditionDto update(UpdateSurveyEditionDto updateSurveyEditionDto, Long aLong) {
+        return null;
+    }
+
+    @Override
+    public List<SurveyEditionDto> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void delete(Long aLong) {
+
+    }
+}
