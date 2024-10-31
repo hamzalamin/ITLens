3d54aff3c75bd7d2f7092ec88dfba41b27478c19
hamzalamin <hamzalamin80@gmail.com>
2024-10-31 11:08:26 +0100

    feat: Add findAllSurveyEditions endpoint
    - Implemented GET /survey/edition to retrieve all survey editions.
    - Utilized ISurveyEditionService to fetch the list of survey editions.

--- src/main/java/com/wora/itlens/controllers/SurveyEditionController.java
+++ src/main/java/com/wora/itlens/controllers/SurveyEditionController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/survey/edition")
@@ -21,5 +20,11 @@ public class SurveyEditionController {
     @PostMapping
     public ResponseEntity<SurveyEditionDto> createSurveyEdition(@RequestBody @Valid CreateSurveyEditionDto createSurveyEditionDto){
         return new ResponseEntity<>(surveyEditionService.save(createSurveyEditionDto), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SurveyEditionDto>> findAllSurveyEditions(){
+        List<SurveyEditionDto> surveyEditionDtoList = surveyEditionService.findAll();
+        return new ResponseEntity<>(surveyEditionDtoList, HttpStatus.OK);
     }
 }
