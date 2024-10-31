d6d5c6ca87896feaef318132141cd1793fac4067
hamzalamin <hamzalamin80@gmail.com>
2024-10-31 01:40:42 +0100

    feat: Add SurveyController with createSurvey endpoint
    - Implemented SurveyController to handle HTTP requests for Survey entity.
    - Added POST endpoint to create a new survey with validation for input data.
    - Utilizes SurveyService to save the Survey and return the created SurveyDto.

---
+++ src/main/java/com/wora/itlens/controllers/SurveyController.java
@@ -0,0 +1,28 @@
+package com.wora.itlens.controllers;
+
+import com.wora.itlens.mappers.SurveyMapper;
+import com.wora.itlens.models.dtos.surveys.CreateSurveyDto;
+import com.wora.itlens.models.dtos.surveys.SurveyDto;
+import com.wora.itlens.services.impl.SurveyService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/surveys")
+@RequiredArgsConstructor
+public class SurveyController {
+
+    private final SurveyService surveyService;
+
+    @PostMapping
+    public ResponseEntity<SurveyDto> createSurvey(@RequestBody @Valid CreateSurveyDto createSurveyDto){
+        return new ResponseEntity<>(surveyService.save(createSurveyDto), HttpStatus.OK);
+    }
+
+
+}
