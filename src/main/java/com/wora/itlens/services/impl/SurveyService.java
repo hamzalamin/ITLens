ccb4a46530b065c07bd90ac7aa0236569d5ade23
hamzalamin <hamzalamin80@gmail.com>
2024-10-31 01:19:26 +0100

    feat: Implement update method in SurveyService
    - Added implementation of the update method to modify an existing Survey.
    - Retrieved the Survey by ID, throwing an EntityNotFoundException if not found.
    - Updated the title and description of the Survey based on the provided UpdateSurveyDto.
    - Returned the updated SurveyDto after saving the changes.

--- src/main/java/com/wora/itlens/services/impl/SurveyService.java
+++ src/main/java/com/wora/itlens/services/impl/SurveyService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -37,12 +38,17 @@ public class SurveyService implements ISurveyService {
 
     @Override
     public SurveyDto update(UpdateSurveyDto updateSurveyDto, Long id) {
-        return null;
+        Survey survey = surveyRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Survey", id));
+
+        survey.setTitle(updateSurveyDto.title());
+        survey.setDescription(updateSurveyDto.description());
+        Survey updatedSurvey = surveyRepository.save(survey);
+        return surveyMapper.toDto(updatedSurvey);
     }
 
     @Override
-    public List<SurveyDto> findAll() {
-        return List.of();
+
     }
 
     @Override
