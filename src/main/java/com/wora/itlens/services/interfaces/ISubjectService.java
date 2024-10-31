08e4bc6778054a273bbfd4c18a90606c50c28377
hamzalamin <hamzalamin80@gmail.com>
2024-10-31 13:34:56 +0100

    Add ISubjectService interface: Defines contract for Subject service with CRUD operations via GenericService extension

---
+++ src/main/java/com/wora/itlens/services/interfaces/ISubjectService.java
@@ -0,0 +1,9 @@
+package com.wora.itlens.services.interfaces;
+
+import com.wora.itlens.models.dtos.subjects.CreateSubjectDto;
+import com.wora.itlens.models.dtos.subjects.SubjectDto;
+import com.wora.itlens.models.dtos.subjects.UpdateSubjectDto;
+import com.wora.itlens.services.GenericService;
+
+public interface ISubjectService extends GenericService<CreateSubjectDto, UpdateSubjectDto, SubjectDto, Long> {
+}
