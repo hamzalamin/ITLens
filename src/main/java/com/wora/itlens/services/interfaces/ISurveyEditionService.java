97a1e299485f6694f6ffea3afe4f4e4970db9171
hamzalamin <hamzalamin80@gmail.com>
2024-10-31 09:04:45 +0100

    feat: Add ISurveyEditionService interface for survey editions management
    - Defined ISurveyEditionService interface extending GenericService with CreateSurveyEditionDto, UpdateSurveyEditionDto, and SurveyEditionDto.
    - Added CRUD methods for handling survey edition entities.

---
+++ src/main/java/com/wora/itlens/services/interfaces/ISurveyEditionService.java
@@ -0,0 +1,9 @@
+package com.wora.itlens.services.interfaces;
+
+import com.wora.itlens.models.dtos.surveyEditions.CreateSurveyEditionDto;
+import com.wora.itlens.models.dtos.surveyEditions.SurveyEditionDto;
+import com.wora.itlens.models.dtos.surveyEditions.UpdateSurveyEditionDto;
+import com.wora.itlens.services.GenericService;
+
+public interface ISurveyEditionService extends GenericService<CreateSurveyEditionDto, UpdateSurveyEditionDto, SurveyEditionDto, Long> {
+}
