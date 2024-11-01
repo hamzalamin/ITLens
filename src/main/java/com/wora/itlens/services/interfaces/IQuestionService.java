195b341e9dcf4c97d4901635ec47a30851fdec35
hamzalamin <hamzalamin80@gmail.com>
2024-11-01 08:16:31 +0100

    feat: Define IQuestionService interface for Question entity management
    - Created IQuestionService extending GenericService.
    - Specifies generic CRUD operations with CreateQuestionDto, UpdateQuestionDto, QuestionsDto, and Long ID type.
    - Establishes service layer contract for Question entity management.

---
+++ src/main/java/com/wora/itlens/services/interfaces/IQuestionService.java
@@ -0,0 +1,9 @@
+package com.wora.itlens.services.interfaces;
+
+import com.wora.itlens.models.dtos.questions.CreateQuestionDto;
+import com.wora.itlens.models.dtos.questions.QuestionsDto;
+import com.wora.itlens.models.dtos.questions.UpdateQuestionDto;
+import com.wora.itlens.services.GenericService;
+
+public interface IQuestionService extends GenericService<CreateQuestionDto, UpdateQuestionDto, QuestionsDto, Long> {
+}
