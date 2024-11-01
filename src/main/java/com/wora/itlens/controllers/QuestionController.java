113856b3c981b84789ef9fd8a83d2e1370597c1d
hamzalamin <hamzalamin80@gmail.com>
2024-11-01 09:59:34 +0100

    feat: Implement findAll endpoint in QuestionController
    - Added GET mapping to retrieve a list of all questions.
    - Returns a list of QuestionsDto with HTTP status 200 (OK).

--- src/main/java/com/wora/itlens/controllers/QuestionController.java
+++ src/main/java/com/wora/itlens/controllers/QuestionController.java
@@ -8,10 +8,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/questions")
@@ -23,6 +22,12 @@ public class QuestionController {
     @PostMapping
     public ResponseEntity<QuestionsDto> createQuestion(@RequestBody @Valid CreateQuestionDto createQuestionDto){
         return new ResponseEntity<>(questionService.save(createQuestionDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<QuestionsDto>> findAll(){
+        List<QuestionsDto> questionsDtoList = questionService.findAll();
+        return new ResponseEntity<>(questionsDtoList, HttpStatus.OK);
     }
 
 }
