940b7b48cea136f71367ba32d8db487c9963a9b6
hamzalamin <hamzalamin80@gmail.com>
2024-11-01 08:11:20 +0100

    feat: Implement QuestionMapper interface for mapping Question entities and DTOs
    - Added QuestionMapper interface extending GenericMapper for mapping Question entities and DTOs.
    - Included methods to convert between Question, QuestionsDto, CreateQuestionDto, and UpdateQuestionDto.
    - Configured with MapStruct for Spring dependency injection.

---
+++ src/main/java/com/wora/itlens/mappers/QuestionMapper.java
@@ -0,0 +1,16 @@
+package com.wora.itlens.mappers;
+
+import com.wora.itlens.mappers.api.GenericMapper;
+import com.wora.itlens.models.dtos.questions.CreateQuestionDto;
+import com.wora.itlens.models.dtos.questions.QuestionsDto;
+import com.wora.itlens.models.dtos.questions.UpdateQuestionDto;
+import com.wora.itlens.models.entites.Question;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface QuestionMapper extends GenericMapper<Question, QuestionsDto> {
+    Question toEntity(QuestionsDto questionsDto);
+    Question toEntity(CreateQuestionDto questionsDto);
+    Question toEntity(UpdateQuestionDto questionsDto);
+    QuestionsDto toDto(Question question);
+}
