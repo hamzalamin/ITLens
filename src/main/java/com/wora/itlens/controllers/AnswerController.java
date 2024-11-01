522d5f60fd7e63de437c7b4d60993fb5067fbb86
hamzalamin <hamzalamin80@gmail.com>
2024-11-01 15:05:07 +0100

    Fix: Add final modifier to answerService in AnswerController for dependency injection

---
+++ src/main/java/com/wora/itlens/controllers/AnswerController.java
@@ -0,0 +1,53 @@
+package com.wora.itlens.controllers;
+
+
+import com.wora.itlens.models.dtos.answers.AnswerDto;
+import com.wora.itlens.models.dtos.answers.CreateAnswerDto;
+import com.wora.itlens.models.dtos.answers.UpdateAnswerDto;
+import com.wora.itlens.services.interfaces.IAnswerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+public class AnswerController {
+
+    private IAnswerService answerService;
+
+    @PostMapping
+    public ResponseEntity<AnswerDto> createAnswer(@RequestBody @Valid CreateAnswerDto createAnswerDto){
+        return new ResponseEntity<>(answerService.save(createAnswerDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AnswerDto> findById(@PathVariable Long id){
+        return new ResponseEntity<>(answerService.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AnswerDto>> findAll(){
+        List<AnswerDto> answerDtoList = answerService.findAll();
+        return new ResponseEntity<>(answerDtoList, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AnswerDto> update(
+            @PathVariable Long id,
+            @RequestBody @Valid UpdateAnswerDto updateAnswerDto
+    )
+    {
+        return new ResponseEntity<>(answerService.update(updateAnswerDto, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        answerService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
