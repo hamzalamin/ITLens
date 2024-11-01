2445adb0ac1e2e44b9875884b8c36e884d3bef76
hamzalamin <hamzalamin80@gmail.com>
2024-11-01 09:57:29 +0100

    feat: Implement createQuestion endpoint in QuestionController
    - Added POST mapping for creating a new question.
    - Validates and processes CreateQuestionDto using IQuestionService.
    - Returns created question with HTTP status 201 (Created).

--- src/main/java/com/wora/itlens/controllers/QuestionController.java
+++ src/main/java/com/wora/itlens/controllers/QuestionController.java
@@ -1,8 +1,15 @@
 package com.wora.itlens.controllers;
 
 
+import com.wora.itlens.models.dtos.questions.CreateQuestionDto;
+import com.wora.itlens.models.dtos.questions.QuestionsDto;
 import com.wora.itlens.services.interfaces.IQuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuestionController {
 
     private final IQuestionService questionService;
+
+    @PostMapping
+    public ResponseEntity<QuestionsDto> createQuestion(@RequestBody @Valid CreateQuestionDto createQuestionDto){
+        return new ResponseEntity<>(questionService.save(createQuestionDto), HttpStatus.CREATED);
+    }
 
 }
