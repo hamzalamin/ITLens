0dabb730c759dec332c88be949e63348d913f637
hamzalamin <hamzalamin80@gmail.com>
2024-11-01 13:42:24 +0100

    Fix: Correct circular dependency in AnswerService by replacing IAnswerService with AnswerRepository and implementing save method with answerMapper

---
+++ src/main/java/com/wora/itlens/repositories/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.wora.itlens.repositories;
+
+import com.wora.itlens.models.entites.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+}
--- src/main/java/com/wora/itlens/services/impl/AnswerService.java
+++ src/main/java/com/wora/itlens/services/impl/AnswerService.java
@@ -1,23 +1,37 @@
 package com.wora.itlens.services.impl;
 
+import com.wora.itlens.exceptions.EntityNotFoundException;
+import com.wora.itlens.mappers.AnswerMapper;
 import com.wora.itlens.models.dtos.answers.AnswerDto;
 import com.wora.itlens.models.dtos.answers.CreateAnswerDto;
 import com.wora.itlens.models.dtos.answers.UpdateAnswerDto;
+import com.wora.itlens.models.entites.Answer;
+import com.wora.itlens.repositories.AnswerRepository;
 import com.wora.itlens.services.interfaces.IAnswerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AnswerService implements IAnswerService {
+
+    private final AnswerRepository answerRepository;
+    private final AnswerMapper answerMapper;
+
     @Override
     public AnswerDto save(CreateAnswerDto createAnswerDto) {
-        return null;
+        Answer answer = answerMapper.toEntity(createAnswerDto);
+        Answer savedAnswer = answerRepository.save(answer);
+        return answerMapper.toDto(savedAnswer);
     }
 
     @Override
     public AnswerDto findById(Long id) {
-        return null;
+        Answer answer = answerRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Answer", id));
+        return answerMapper.toDto(answer);
     }
 
     @Override
