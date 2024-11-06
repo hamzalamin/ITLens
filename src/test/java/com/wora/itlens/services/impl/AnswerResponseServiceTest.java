71e19296e029ce66fef1c6661d18949fa14c940b
hamzalamin <hamzalamin80@gmail.com>
2024-11-06 16:51:32 +0100

    TEST: AnswerResponseServiceTest/processMultipleQuestionsAndAnswers  Should increment selection and answer counts

--- src/test/java/com/wora/itlens/services/impl/AnswerResponseServiceTest.java
+++ src/test/java/com/wora/itlens/services/impl/AnswerResponseServiceTest.java
@@ -1,14 +1,17 @@
 package com.wora.itlens.services.impl;
 
 import com.wora.itlens.mappers.AnswerResponseMapper;
-import com.wora.itlens.models.dtos.answerResponses.AnswerResponseDto;
+import com.wora.itlens.models.dtos.answerResponses.CreateMultipleAnswersAndMultipleResponsesDto;
 import com.wora.itlens.models.dtos.answerResponses.CreateMultipleAnswersAndOneQuestionDto;
 import com.wora.itlens.models.dtos.answerResponses.QuestionWithAnswersResponseDto;
 import com.wora.itlens.models.dtos.answers.AnswerDto;
 import com.wora.itlens.models.dtos.answers.EmbeddedAnswerDto;
-import com.wora.itlens.models.dtos.questions.EmbeddedQuestionDto;
+import com.wora.itlens.models.dtos.questions.QuestionsDto;
+import com.wora.itlens.models.dtos.subjects.EmbeddedSubjectDto;
+import com.wora.itlens.models.dtos.surveyEditions.EmbeddedSurveyEditionsDto;
 import com.wora.itlens.models.entites.Answer;
 import com.wora.itlens.models.entites.Question;
+import com.wora.itlens.models.entites.Subject;
 import com.wora.itlens.models.enumes.QuestionType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +20,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -114,6 +118,57 @@ class AnswerResponseServiceTest {
         verifyNoMoreInteractions(questionService);
     }
 
+    @Test
+    @DisplayName("processMultipleQuestionsAndAnswers() Should increment selection and answer counts")
+    void processMultipleQuestionsAndAnswers_shouldIncrementCountsSuccessfully() {
+
+        EmbeddedSurveyEditionsDto surveyEditionDto = new EmbeddedSurveyEditionsDto(
+                1L,
+                LocalDate.now().plusDays(1),
+                LocalDate.now().plusDays(2),
+                LocalDate.now().plusDays(3)
+        );
+
+        EmbeddedSubjectDto subjectDto = new EmbeddedSubjectDto(1L, "Subject Title", surveyEditionDto);
+
+        List<AnswerDto> answerDtos = List.of(
+                new AnswerDto(1L, "Answer 1", 1),
+                new AnswerDto(2L, "Answer 2", 2)
+        );
+
+        List<EmbeddedAnswerDto> embeddedAnswerDto = List.of(
+                new EmbeddedAnswerDto(1L, "hhhh",2),
+                new EmbeddedAnswerDto(1L, "hhhh",2)
+        );
 
 
+        List<QuestionsDto> questionsDtos = List.of(
+                new QuestionsDto(1L, "Question Title", 1, QuestionType.MULTIPLE_CHOICE, subjectDto, embeddedAnswerDto)
+        );
+
+        CreateMultipleAnswersAndMultipleResponsesDto dto = new CreateMultipleAnswersAndMultipleResponsesDto(answerDtos, questionsDtos);
+
+        Question mockQuestion = new Question();
+        mockQuestion.setAnswerCount(2);
+
+        Answer mockAnswer1 = new Answer();
+        mockAnswer1.setSelectionCount(3);
+
+        Answer mockAnswer2 = new Answer();
+        mockAnswer2.setSelectionCount(4);
+
+        when(questionService.getQuestionEntity(anyLong())).thenReturn(mockQuestion);
+        when(answerService.getAnswerEntity(1L)).thenReturn(mockAnswer1);
+        when(answerService.saveAnswerEntity(any(Answer.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(questionService.saveQuestionEntity(any(Question.class))).thenReturn(mockQuestion);
+
+        List<QuestionWithAnswersResponseDto> responses = answerResponseService.processMultipleQuestionsAndAnswers(dto);
+
+        assertEquals(1, responses.size());
+        QuestionWithAnswersResponseDto response = responses.getFirst();
+
+        assertEquals(4, mockQuestion.getAnswerCount());
+        assertEquals(5, mockAnswer1.getSelectionCount());
+        assertEquals(4, mockAnswer2.getSelectionCount());
+    }
 }
