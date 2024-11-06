630816281930f157261fb0323ba4eda5dd8fc98d
hamzalamin <hamzalamin80@gmail.com>
2024-11-06 20:56:53 +0100

    Add integration test for GET /owners endpoint. Ensure that 'Hamza' owner is returned and JSON response is correctly structured.

--- src/test/java/com/wora/itlens/owner/controller/OwnerController.java
+++ src/test/java/com/wora/itlens/owner/controller/OwnerController.java
@@ -3,8 +3,8 @@ package com.wora.itlens.owner.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wora.itlens.exceptions.EntityNotFoundException;
 import com.wora.itlens.mappers.OwnerMapper;
-import com.wora.itlens.mappers.OwnerMapperImpl;
 import com.wora.itlens.models.dtos.owners.CreateOwnerDto;
+import com.wora.itlens.models.dtos.owners.OwnerDto;
 import com.wora.itlens.models.entites.Owner;
 import com.wora.itlens.repositories.OwnerRepository;
 import org.junit.jupiter.api.Test;
@@ -14,8 +14,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -51,6 +54,16 @@ public class OwnerController {
 
         assertNotNull(foundOwner);
         assertEquals("Hamza", foundOwner.getName());
+    }
+
+    @Test
+    void testGetAllOwnerIntegration() throws Exception{
+        List<Owner> owners = ownerRepository.findAll().stream()
+                .toList();
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Hamza"));
+
     }
 
 
