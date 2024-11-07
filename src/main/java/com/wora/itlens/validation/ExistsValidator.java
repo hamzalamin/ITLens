7e8495d1a4e788e79a433715b33eae2bbd8a92c7
hamzalamin <hamzalamin80@gmail.com>
2024-11-07 17:48:24 +0100

    Implement ExistsValidator for entity existence check

--- src/main/java/com/wora/itlens/validation/ExistsValidator.java
+++ src/main/java/com/wora/itlens/validation/ExistsValidator.java
@@ -1,4 +1,36 @@
 package com.wora.itlens.validation;
 
-public class ExistsValidator  {
+import com.wora.itlens.annotation.Exists;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ExistsValidator implements ConstraintValidator<Exists, Long> {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private Class<?> entity;
+    private String field;
+
+    @Override
+    public void initialize(Exists constraintAnnotation){
+        this.entity = constraintAnnotation.entity();
+        this.field = constraintAnnotation.field();
+    }
+
+    @Override
+    public boolean isValid(Long id, ConstraintValidatorContext context){
+        if (id == null) return true;
+        String queryStr = "SELECT count(e) FROM " + entity.getName() + " e WHERE e." + field + " = :id";
+        Query query = entityManager.createQuery(queryStr);
+        query.setParameter("id" , id);
+        Long count = (Long) query.getSingleResult();
+        return count != null && count > 0;
+    }
 }
