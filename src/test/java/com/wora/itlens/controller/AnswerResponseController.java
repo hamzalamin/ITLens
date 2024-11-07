f90e54fa8b12eac0aee1b5f3377d65e622fc0994
hamzalamin <hamzalamin80@gmail.com>
2024-11-07 02:07:49 +0100

    Add test for saveUserAnswer endpoint in AnswerController

---
+++ src/test/java/com/wora/itlens/controller/AnswerResponseController.java
@@ -0,0 +1,63 @@
+package com.wora.itlens.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wora.itlens.models.dtos.answerResponses.CreateAnswerResponseDto;
+import com.wora.itlens.models.entites.Answer;
+import com.wora.itlens.models.entites.Question;
+import com.wora.itlens.repositories.AnswerRepository;
+import com.wora.itlens.repositories.QuestionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AnswerResponseController {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testSaveUserAnswer_Successful() throws Exception {
+        Question question = new Question();
+        question.setText("Sample Question");
+        question.setAnswerCount(0);
+        questionRepository.save(question);
+
+        Answer answer = new Answer();
+        answer.setQuestion(question);
+        answer.setSelectionCount(0);
+        answerRepository.save(answer);
+
+        CreateAnswerResponseDto dto = new CreateAnswerResponseDto(answer.getId(), question.getId());
+        String requestJson = objectMapper.writeValueAsString(dto);
+
+        mockMvc.perform(post("/api/answers/response")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.answer.id").value(answer.getId()))
+                .andExpect(jsonPath("$.question.id").value(question.getId()))
+                .andExpect(jsonPath("$.answer.selectionCount").value(answer.getSelectionCount() + 1))
+                .andExpect(jsonPath("$.question.answerCount").value(question.getAnswerCount() + 1));
+
+    }
+}
