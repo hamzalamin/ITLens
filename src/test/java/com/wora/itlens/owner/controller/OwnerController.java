3838ba72631d1b1b8ba40e4af9fbd9ea4fc33f25
hamzalamin <hamzalamin80@gmail.com>
2024-11-11 08:38:23 +0100

    Refactor/ use the testprofiles for integration tests

--- src/test/java/com/wora/itlens/owner/controller/OwnerController.java
+++ src/test/java/com/wora/itlens/owner/controller/OwnerController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class OwnerController {
 
     @Autowired
